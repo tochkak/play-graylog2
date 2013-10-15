1dca9922c11a33b9b61462bbad99cca63352f71f
Kay Roepke <kroepke@googlemail.com>
2013-10-15 15:10:01 +0200

    add chunked result and send access logs by default

--- samples/java-logging/app/controllers/Application.java
+++ samples/java-logging/app/controllers/Application.java
@@ -1,10 +1,10 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
@@ -14,6 +14,20 @@ public class Application extends Controller {
     	log.info("index3");
     	log.info("index4");
         return ok(index.render("Your new application is ready."));
+    }
+
+    public static Result chunked() {
+        final String[] strings = new String[] {"abc", "cde", "efg"};
+        Chunks<String> chunks = new StringChunks() {
+            @Override
+            public void onReady(Out<String> out) {
+                for (String string : strings) {
+                    out.write(string);
+                }
+                out.close();
+            }
+        };
+        return ok(chunks);
     }
 
 }
